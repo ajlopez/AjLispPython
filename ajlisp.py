08db7b2d552e1dafeb9ee541924270ee6a98ae73
ajlopez <webmaster@ajlopez.com>
2013-06-23 18:10:10 -0300

    Evaluate symbol in context, with test

--- ajlisp.py
+++ ajlisp.py
@@ -1,4 +1,9 @@
 
+from list import List
+from context import Context
+from symbol import Symbol
 
 def evaluate(arg, context = None):
+    if isinstance(arg, Symbol):
+        return arg.evaluate(context)
     return arg
--- test_evaluate.py
+++ test_evaluate.py
@@ -10,5 +10,11 @@ class EvaluateTests(unittest.TestCase):
     def test_evaluate_string(self):
         self.assertEqual("spam", ajlisp.evaluate("spam"))
         
+    def test_evaluate_symbol_in_context(self):
+        context = ajlisp.Context()
+        symbol = ajlisp.Symbol('one')
+        context.set('one', 1)
+        self.assertEqual(1, ajlisp.evaluate(symbol, context))
+        
 if __name__ == '__main__':
     unittest.main()
