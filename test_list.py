fb342bc88e6dd426e70b7bc31041f4f2924965c0
ajlopez <webmaster@ajlopez.com>
2013-06-23 16:52:09 -0300

    Additional test for list

--- test_list.py
+++ test_list.py
@@ -7,7 +7,16 @@ class ListTests(unittest.TestCase):
     def test_create_list(self):
         list = List(1, None)
         self.assertEqual(1, list.head())
-        self.assertEqual(None, list.tail())
+        self.assertIsNone(list.tail())
+        
+    def test_create_list_with_two_elements(self):
+        list = List(1, List(2, None))
+        self.assertEqual(1, list.head())
+        self.assertIsNotNone(list.tail())
+        tail = list.tail()
+        self.assertEqual(2, tail.head())
+        self.assertIsNone(tail.tail())
         
 if __name__ == '__main__':
     unittest.main()
+    
